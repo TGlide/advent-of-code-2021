cb2c20272b20fbaba419c19173af22836c5f3c1b
tglide <26071571+TGlide@users.noreply.github.com>
2021-12-07 00:43:33 +0000

    refactor: folder structure

--- solutions/utils.py
+++ solutions/utils.py
@@ -2,7 +2,7 @@ def getInputFileLines(file_number: str) -> list:
     import os
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    input_file = os.path.join(current_dir, f'inputs/{file_number}.txt')
+    input_file = os.path.join(current_dir, f'../inputs/{file_number}.txt')
 
     with open(input_file) as f:
         lines = f.readlines()
